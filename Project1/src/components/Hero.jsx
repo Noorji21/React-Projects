f911c4cc4edf58581e04bc03dc962198cb2f798c
Noorji21 <singhha19@gmail.com>
2024-07-31 20:21:27 +0200

    first-project

--- Project1/src/components/Hero.jsx
+++ Project1/src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className="hero-content">
         <h1>YOUR FEET DESERVE THE BEST</h1>
         <p>
-          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          YOUR FEET DESERVE THE BEST ANND WE’RE HERE TO HELP YOU WITH OUR
           SHOES. YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
           SHOES.
         </p>
